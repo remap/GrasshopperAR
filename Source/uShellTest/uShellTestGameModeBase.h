274a9de1711f2c1aa5df12a72da33773a8f48477
peetonn <gpeetonn@gmail.com>
2020-02-04 09:34:04 -0800

    initial

---
+++ Source/uShellTest/uShellTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "uShellTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class USHELLTEST_API AuShellTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
